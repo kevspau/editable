af09165c113937fbf742bf7e1f4563628a9431a5
csharpdf <csharpdfjer@gmail.com>
2022-01-10 19:51:58 +0000

    joe mama

--- main.nim
+++ main.nim
@@ -1,1 +1,8 @@
 
+import std/[dom]
+
+let x = $(window.prompt("whos joe mama", ""))
+if x != "":
+    document.write("<h1>" & x & "</h1>")
+    if x == "barack obama":
+        document.write("nigger")
